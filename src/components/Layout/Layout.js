14201dd504161638c6f283ba026c985b4b9b3412
benceb1 <bence.b1.1.2@gmail.com>
2020-02-08 11:58:44 +0100

    cuccok

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,34 @@
+import React, { Fragment, Component } from 'react';
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    };
+
+    showSideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    };
+
+    sideDrawerToggleHandler = () => {
+        this.setState(prevState => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
+    };
+
+    render() {
+        return (
+            <Fragment>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.showSideDrawerClosedHandler}
+                />
+                <main className={classes.Content}>{this.props.children}</main>
+            </Fragment>
+        );
+    }
+}
+export default Layout;
